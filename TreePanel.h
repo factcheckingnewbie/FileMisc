da675edb885414b6fc9fea197df6c53bb98070f1
factcheckingnewbie <200551791+factcheckingnewbie@users.noreply.github.com>
2025-06-18 12:41:20 +0200

    FirstMinimalMileStone,Treeview Expands to home and can no be fully retracted by user:  CMakeLists.txt 	FirstMinimalMileStone,Treeview Expands to home and can no be fully retracted by user:  FilePanel.cpp 	FirstMinimalMileStone,Treeview Expands to home and can no be fully retracted by user:  FilePanel.h 	FirstMinimalMileStone,Treeview Expands to home and can no be fully retracted by user:  README.md 	FirstMinimalMileStone,Treeview Expands to home and can no be fully retracted by user:  TreePanel.cpp 	FirstMinimalMileStone,Treeview Expands to home and can no be fully retracted by user:  TreePanel.h 	FirstMinimalMileStone,Treeview Expands to home and can no be fully retracted by user:  main.cpp

---
+++ TreePanel.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <QWidget>
+#include <QTreeView>
+#include <KDirModel>
+#include <QUrl>
+
+/*
+ * TreePanel - extracted from main.cpp
+ * Contains a QTreeView with KDirModel, all signals/slots and settings as in main.cpp as of commit d3a744174bba5d55270d3b4313401428a8a62a28.
+ */
+class TreePanel : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit TreePanel(QWidget *parent = nullptr);
+
+    QTreeView* view() const { return m_treeView; }
+    KDirModel* model() const { return m_treeModel; }
+    QUrl rootUrl() const { return m_treeRootUrl; }
+
+signals:
+    void directorySelected(const QUrl &url);
+
+private:
+    QTreeView *m_treeView;
+    KDirModel *m_treeModel;
+    QUrl m_treeRootUrl;
+};
