8564d7c26d924dd0ec3bd381aa557884dcdd0975
factcheckingnewbie <200551791+factcheckingnewbie@users.noreply.github.com>
2025-06-18 18:27:14 +0200

    modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <QSplitter>
 #include <QDir>
 #include <QDebug>
+#include <QListView>
 #include "FilePanel.h"
 #include "TreePanel.h"
 
@@ -21,7 +22,7 @@ int main(int argc, char *argv[])
     FilePanel *leftPanel = new FilePanel;
     FilePanel *rightPanel = new FilePanel;
 
-    // Start in home and root for demonstration; adapt as you wish.
+    // Start in home and root for demonstration
     leftPanel->setDirectory(QUrl::fromLocalFile(QDir::homePath()));
     rightPanel->setDirectory(QUrl::fromLocalFile(QDir::rootPath()));
 
@@ -29,9 +30,54 @@ int main(int argc, char *argv[])
     splitter->addWidget(leftPanel);
     splitter->addWidget(rightPanel);
 
-    // Directory navigation: Connect TreePanel to left FilePanel
+    // Track active panel
+    FilePanel *activePanel = leftPanel;
+    
+    class FocusFilter : public QObject {
+    public:
+        FilePanel *&active;
+        FilePanel *left;
+        FilePanel *right;
+        
+        FocusFilter(FilePanel *&activeRef, FilePanel *l, FilePanel *r, QObject *parent = nullptr) 
+            : QObject(parent), active(activeRef), left(l), right(r) {}
+        
+        bool eventFilter(QObject *obj, QEvent *event) override {
+            if (event->type() == QEvent::FocusIn) {
+                if (obj == left || obj->parent() == left) {
+                    qDebug() << "Left panel activated";
+                    active = left;
+                } else if (obj == right || obj->parent() == right) {
+                    qDebug() << "Right panel activated";
+                    active = right;
+                }
+            }
+            return false;
+        }
+    };
+    
+    // Create filter with mainWin as parent (auto-deleted)
+    FocusFilter *filter = new FocusFilter(activePanel, leftPanel, rightPanel, &mainWin);
+    
+    leftPanel->installEventFilter(filter);
+    rightPanel->installEventFilter(filter);
+    
+    // Safely install on list views if they exist
+    if (QListView *leftList = leftPanel->findChild<QListView*>()) {
+        leftList->installEventFilter(filter);
+    }
+    if (QListView *rightList = rightPanel->findChild<QListView*>()) {
+        rightList->installEventFilter(filter);
+    }
+    
+    // Connect TreePanel to update active panel
     QObject::connect(treePanel, &TreePanel::directorySelected,
-                     leftPanel, &FilePanel::setDirectory);
+                     [&activePanel](const QUrl &url) {
+                         if (activePanel) {
+                             qDebug() << "TreePanel updating active panel to:" << url.toString();
+                             activePanel->setDirectory(url);
+                         }
+                     });
 
     QHBoxLayout *layout = new QHBoxLayout;
     layout->addWidget(splitter);
